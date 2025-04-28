a8f94ef5c6d4abf010f2103c7291f0bf2b1567e0
Nyannn915 <a2081149998@gmail.com>
2025-04-28 14:19:11 +0800

    suit the upper and lowe case

--- task1.py
+++ task1.py
@@ -1,24 +1,23 @@
 def calculate_value(board):
-    result = 0
+    sum_score = 0
     for row in board:
         for letter in row:
-            if letter in ['E', 'A', 'I', 'O', 'N', 'R', 'T', 'L', 'S', 'U']:
-                result = result + 1
-            elif letter in ['D', 'G']:
-                result = result + 2
-            elif letter in ['B', 'C', 'M', 'P']:
-                result = result + 3
-            elif letter in ['F', 'H', 'V', 'W', 'Y']:
-                result = result + 4
-            elif letter == 'K':
-                result = result + 5
-            elif letter in ['J', 'X']:
-                result = result + 8
-            elif letter in ['Q', 'Z']:
-                result = result + 10
+            if letter in ['E', 'e', 'A', 'a', 'I', 'i', 'O', 'o', 'N', 'n', 'R', 'r', 'T', 't', 'L', 'l', 'S', 's', 'U', 'u']:
+                sum_score = sum_score + 1
+            elif letter in ['D', 'd', 'G', 'g']:
+                sum_score = sum_score + 2
+            elif letter in ['B', 'b', 'C', 'c', 'M', 'm', 'P', 'p']:
+                sum_score = sum_score + 3
+            elif letter in ['F', 'f', 'H', 'h', 'V', 'v', 'W', 'w', 'Y', 'y']:
+                sum_score = sum_score + 4
+            elif letter in ['K', 'k']:
+                sum_score = sum_score + 5
+            elif letter in ['J', 'j', 'X', 'x']:
+                sum_score = sum_score + 8
+            elif letter in ['Q', 'q', 'Z', 'z']:
+                sum_score = sum_score + 10
             else:
-                result = result + 0
-    return result
+                sum_score = sum_score + 0
+    return sum_score
 
-# 测试
-print(calculate_value([["Q", "Z"], ["K", "?"]]))      
+print(calculate_value([["a", "G"], ["C", "k"]]))
