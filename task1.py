6ebf4510060a76fd869bd8943e3bbcd4966c4227
Nyannn915 <a2081149998@gmail.com>
2025-04-28 13:39:12 +0800

    first try

--- task1.py
+++ task1.py
@@ -1,7 +1,25 @@
-#初始化calculate_value函数结构，add simple structer 
 def calculate_value(board):
-    total = 0
+    result = 0
+    for row in board:
+        for letter in row:
+            if letter in ['E', 'A', 'I', 'O', 'N', 'R', 'T', 'L', 'S', 'U']:
+                result = result + 1
+            elif letter in ['D', 'G']:
+                result = result + 2
+            elif letter in ['B', 'C', 'M', 'P']:
+                result = result + 3
+            elif letter in ['F', 'H', 'V', 'W', 'Y']:
+                result = result + 4
+            elif letter == 'K':
+                result = result + 5
+            elif letter in ['J', 'X']:
+                result = result + 8
+            elif letter in ['Q', 'Z']:
+                result = result + 10
+            else:
+                result = result + 0
+    return result
 
-    return total
-
-print(calculate_value([["A", "B"], ["C", "D"]]))  
+# 测试
+print(calculate_value([["A"]]))         
+print(calculate_value([["Z"]]))         
