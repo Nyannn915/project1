abb90c566550a8ef37dd4499c7e094db6485aae4
Nyannn915 <a2081149998@gmail.com>
2025-04-28 18:21:44 +0800

    counter doesnt work, ,,

--- task2.py
+++ task2.py
@@ -1,33 +1,23 @@
 def words_on_board(words, board):
-    all_tiles = []
-    for row in board:
-        for letter in row:
-            all_tiles.append(letter)
-
-    tile_counter = Counter()
-    for ch in all_tiles:
-        tile_counter[ch] += 1
-
+    # blank tile处理思路参考自Stackoom（https://stackoom.com/en/question/3Dz3N）
+    tiles = []
+    for line in board:
+        for tile in line:
+            tiles.append(tile)
+    
     result = []
     for word in words:
-        word_counter = Counter()
-        for c in word:
-            word_counter[c] += 1
-
-        ok = True
-        used_blanks = 0 
-
-        for ch in word_counter:
-            if ch in tile_counter:
-                
-                if tile_counter[ch] >= word_counter[ch]:
-                    continue
-                else:
-                    blanks = tile_counter.get('_', 0) - used_blanks
-                    left_need = word_counter[ch] - tile_counter[ch]
-                    if blanks >= left_need:
-                        used_blanks += left_need
-                    else:
-                        ok = False
-                        break
+        hand = tiles.copy()
+        valid = True
+        for letter in word:
+            if letter in hand:
+                hand.remove(letter)
+            elif '_' in hand:
+                hand.remove('_')
             else:
+                valid = False
+                break
+        if valid:
+            result.append(word)
+    return result
+
