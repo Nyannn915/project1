52357cf2956288e423f62aa77d8f054ab897e8d6
Nyannn915 <a2081149998@gmail.com>
2025-05-02 06:43:21 +0800

    almost

--- task4.py
+++ task4.py
@@ -17,22 +17,17 @@ def textoggle_move(board, word_sequence, spare_letters):
     for col in range(width):
         stack = []
         
-        row_index = height - 1
-        while row_index >= 0:
-            if new_board[row_index][col] is not None:
-                stack.append(new_board[row_index][col])
-            row_index -= 1
+        for row in range(height-1, -1, -1):
+            if new_board[row][col] is not None:
+                stack.append(new_board[row][col])
         
-        
-        row_index = height - 1
-        while row_index >= 0:
+        for row in range(height-1, -1, -1):
             if stack:
-                new_board[row_index][col] = stack.pop(0)
+                new_board[row][col] = stack.pop(0)
             else:
-                new_board[row_index][col] = None
-            row_index -= 1
-        
-        column_result = []
-        for r in range(height):
-            column_result.append(new_board[r][col])   
+                if spare_letters:
+                    new_board[row][col] = spare_letters[0]
+                else:
+                    new_board[row][col] = None
+    
     return new_board
