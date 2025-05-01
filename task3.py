b7a1eb302b22865950d2d30c6c5963a77d490d27
Nyannn915 <a2081149998@gmail.com>
2025-05-01 23:28:17 +0800

    direction

--- task3.py
+++ task3.py
@@ -1,41 +1,62 @@
-def find_word(w, b):
-    n = len(b)
-    m = len(b[0])
-    for r in range(n):
-        for c in range(m):
-            # 检查向下
-            k = 0
-            x = r
-            y = c
-            while k < len(w):
-                if x >= n or b[x][y] != w[k]:
-                    break
-                k += 1
-                x += 1
-            if k == len(w):
-                return True
-
-            # 检查向右
-            kk = 0
-            xx = r
-            yy = c
-            while kk < len(w):
-                if yy >= m or b[xx][yy] != w[kk]:
-                    break
-                kk += 1
-                yy += 1
-            if kk == len(w):
-                
-                return True
-
+def find_word(word, board):
+    n = len(board)
+    m = len(board[0])
     
+    def search(x, y, idx, visited):
+        if idx == len(word):
+            return True
+        
+        if x < 0 or x >= n:
+            return False
+        if y < 0 or y >= m:
+            return False
+        
+        if (x, y) in visited:
+            return False
+        
+        if board[x][y] != word[idx]:
+            return False
+        
+        visited.add((x, y))
+        
+        right = search(x + 1, y, idx + 1, visited)
+        if right:
+            visited.remove((x, y))
+            return True
+        
+        left = search(x - 1, y, idx + 1, visited)
+        if left:
+            visited.remove((x, y))
+            return True
+        
+        down = search(x, y + 1, idx + 1, visited)
+        if down:
+            visited.remove((x, y))
+            return True
+        
+        up = search(x, y - 1, idx + 1, visited)
+        if up:
+            visited.remove((x, y))
+            return True
+        
+        visited.remove((x, y))
+        return False
+    
+    for i in range(n):
+        for j in range(m):
+            visited = set()
+            if search(i, j, 0, visited):
+                print(f"找到了，单词'{word}'起点在({i},{j})。")
+                return True
+    
+    print(f"没找到单词'{word}'。")
     return False
 
-# 简单测试
+# 测试代码
 board = [
-    ['C', 'A', 'T'],
-    ['D', 'O', 'G'],
-    ['P', 'I', 'G']
+    ['A', 'B', 'C', 'E'],
+    ['S', 'F', 'C', 'S'],
+    ['A', 'D', 'E', 'E']
 ]
 
 for word in ['CAT', 'DOG', 'PIG', 'COP']:
