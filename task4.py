8776ad99f8b7422a7c70976a10cb7698dca03463
Nyannn915 <a2081149998@gmail.com>
2025-05-02 04:30:24 +0800

    eliminiate words

--- task4.py
+++ task4.py
@@ -3,10 +3,12 @@ def textoggle_move(board, word_sequence, spare_letters):
     for i in range(len(board)):
         new_row = []
         for j in range(len(board[i])):
-            
             new_row.append(board[i][j])
-        
         new_board.append(new_row)
     
+    for position in word_sequence:
+        row_index = position[0]
+        col_index = position[1]
+        new_board[row_index][col_index] = None
     
     return new_board
