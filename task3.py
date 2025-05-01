df0f7090c3f8435fbd719d2ef1b821da6c9bfb98
Nyannn915 <a2081149998@gmail.com>
2025-05-02 00:20:37 +0800

    DFS method

--- task3.py
+++ task3.py
@@ -1,51 +1,26 @@
 def word_on_board(word, board):
     height = len(board)
     width = len(board[0])
-    
     for row in range(height):
-        for col in range(width):
-            if board[row][col] != word[0]:
-                continue  
-                
-            visited = set()
-            steps = []
-            
-            visited.add((row, col))
-            steps.append((row, col))
-            
-            if search_remaining(word, 1, row, col, board, visited, steps):
-                return steps  
-    
-    return None  
+        for col in range(board[0]):
+            steps = [(row, col)]
+            visited = {(row, col)}
+            if search(word, 1, row, col, board, visited, steps):
+                return steps
+    return None
 
-
-def search_remaining(word, index, row, col, board, visited, steps):
-    if index >= len(word):
+def search(word, index, row, col, board, visited, steps):
+    if index == len(word):
         return True
-    
     directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
-    
-    for direction in directions:
-        dr, dc = direction
-        r = row + dr
-        c = col + dc
-        
-        if r < 0 or r >= len(board) or c < 0 or c >= len(board[0]):
-            continue  
-            
-        if (r, c) in visited:
-            continue
-            
-        if board[r][c] != word[index]:
-            continue
-            
-        visited.add((r, c))
-        steps.append((r, c))
-        
-        if search_remaining(word, index + 1, r, c, board, visited, steps):
-            return True
-            
-        visited.remove((r, c))
-        steps.pop()
-    
+    height, width = len(board), len(board[0])
+    for dr, dc in directions:
+        r, c = row + dr, col + dc
+        if (0 <= r < rows and 0 <= c < cols and (r, c) not in visited and board[r][c] == word[index]):
+            visited.add((r, c))
+            steps.append((r, c))
+            if search(word, index + 1, r, c, board, visited, steps):
+                return True
+            visited.remove((r, c))
+            steps.pop()
     return False
